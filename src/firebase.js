416a75d3eae0c5a109cfc99d2621568143aadff1
najeebib <najeebibraheem1999@hotmail.com>
2023-09-15 12:28:34 +0300

    remove firebse config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-const firebaseConfig = {
-  apiKey: "AIzaSyBJC_pEMNWuFSHdUL9_RZ6aNwUGB2-GgTM",
-  authDomain: "chat-app-38f29.firebaseapp.com",
-  projectId: "chat-app-38f29",
-  storageBucket: "chat-app-38f29.appspot.com",
-  messagingSenderId: "807063966563",
-  appId: "1:807063966563:web:cd4f19c0af738030293956"
+const firebaseConfig = {//put your firebase config here
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = firebase.initializeApp(firebaseConfig);
