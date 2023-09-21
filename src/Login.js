e6dc8933b6ea270071175485e20465860656644e
najeebib <najeebibraheem1999@hotmail.com>
2023-09-21 15:25:44 +0300

    Add proper css to login page

--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,72 @@
-import React from 'react'
+import { React, useState } from 'react'
+
 import "./Login.css"
 import { useNavigate } from 'react-router-dom';
+import {Link} from "react-router-dom"
 
 function Login({ setIsLoggedIn }) {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here, you can add code to send login data to your server or perform authentication.
+    // For this example, we'll just log the data to the console.
+    console.log(formData);
+    setIsLoggedIn(true);
+
+      // Redirect to the chat page
+      navigate('/chat');
+  };
+
+
+
+
+
   const navigate = useNavigate();
 
     //login using google sigin popup with firebase
     const signIn = () => {
-      setIsLoggedIn(true);
-
-      // Redirect to the chat page
-      navigate('/chat');
+      
     }
   return ( 
-    <div className="Login">
-        <div className="LoginContainer">
-            <div className="LoginText">
-                <h1>Sign in to chat app</h1>
-            </div>
+    <div className="login">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <button type="submit" onClick={signIn}>Sign in with google</button> 
-      
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Don't have an account?{' '}
+        <Link to="/register">Register here</Link>
+      </p>
     </div>
   )
 }
